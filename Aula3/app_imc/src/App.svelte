0173940eccc3f37f7fea7b5bc940a955950353ee
JoaoP-Souza <108741673+JoaoP-Souza@users.noreply.github.com>
2023-08-31 00:02:26 -0300

    adiciona o arquivo README, juntamente de um design atualizado para o frontend

--- Aula3/app_imc/src/App.svelte
+++ Aula3/app_imc/src/App.svelte
@@ -24,51 +24,72 @@
 	  } else {
 		imc = 0;
 		faixa = "";
-		faixaObesidade = "";
 	  }
 	}
   </script>
   
   
-  <main>
-	<h1 class='title'>Calculadora de IMC</h1>
+  
+<main>
+	<h1 class = "title">Calculadora de IMC</h1>
+	<h2 class = "description" >Digite sua altura e peso abaixo para calcular o IMC</h2>
 	<label>
 	  Altura (m):
-	  <input type="number" bind:value={altura} step="0.01" />
+	  <input type="number" bind:value={altura} step="0.1" />
 	</label>
 	<label>
 	  Peso (kg):
-	  <input type="number" bind:value={peso} step="0.1" />
+	  <input type="number" bind:value={peso} step="10" />
 	</label>
 	<button on:click={calcularIMC}>Calcular</button>
+  
 	{#if imc > 0}
-	  <p>IMC: {imc.toFixed(2)}</p>
+	  <table class="orange-table">
+		<tr>
+		  <td><strong>IMC:</strong></td>
+		  <td>{imc.toFixed(2)}</td>
+		</tr>
+		<tr>
+		  <td><strong>Faixa:</strong></td>
+		  <td class={faixa === 'Abaixo do peso' ? 'magreza' : faixa === 'Peso normal' ? 'normal' : 'pre-obesidade'}>{faixa}</td>
+		</tr>
+		
+	  </table>
 	{/if}
-	{#if faixa}
-	  <p>Faixa: {faixa}</p>
-	{/if}
-
-	{#if imc > 0}
-  <p>IMC: {imc.toFixed(2)}</p>
-  <p>Faixa: {faixa}</p>
-  <p>Faixa de Obesidade: {faixaObesidade}</p>
-{/if}
   </main>
-
+  
   <style>
-	main {
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  justify-content: center;
-	  height: 100vh;
-	  background-color: #DDDDDD;
+		main {
+		text-align: center;
+	}
+	
+	.title {
+		background-color: red;
+		padding: 10px;
+		border-radius: 5px;
+		margin-bottom: 20px;
+		color: #ffffff
 	}
 
-	.title {
-	display: inline-block;
-    padding: 10px;
-    background-color: rgb(232, 78, 78);
-	color: white;
+	.description {
+		margin-bottom: 20px;
+		font-weight: 1000;
+		font-size: large;
+	}
+	
+	.orange-table {
+	  border-collapse: collapse;
+	  width: 100%;
+	  max-width: 400px;
+	  margin: 20px auto;
+	}
+  
+	.orange-table td {
+	  padding: 8px;
+	  border: 1px solid #fbbf4f;
+	  background-color: #fbbf4f;
+	  color: white;
+	  font-weight: 300;
+	  font-size: large;
 	}
   </style>
